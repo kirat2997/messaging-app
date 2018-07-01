af9c7ddb2d66657940827718e1f188eb332ce267
Harkirat <kirat2997@gmail.com>
2018-07-01 18:02:48 +0530

    mongoose initialized

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
+require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.TEST_DB_URL)
+
+app.set('view engine','html')
+app.use(passport.initialize())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(PORT, () => {
   console.log(`Server up - localhost:${PORT}`)
