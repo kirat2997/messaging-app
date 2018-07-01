27cd4aa0858e2dc3404c461deaf4ff726e4c5448
Harkirat <kirat2997@gmail.com>
2018-07-01 17:59:44 +0530

    login - sinup page

--- client/src/components/Index.vue
+++ client/src/components/Index.vue
@@ -1,15 +1,42 @@
 <template>
-  <div>
-    HELLO
-  </div>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout v-bind="binding" align-center justify-center>
+          <v-flex xs12 sm8 md8>
+            <h2 v-on:click="login = true">Login</h2>
+            <h2 v-on:click="login = false">Register</h2>
+          </v-flex>
+          <v-flex xs12 sm8 md4>
+            <Login v-if="login"/>
+            <Signup v-else/>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
+import Login from './subcomponents/Login.vue'
+import Signup from './subcomponents/Signup.vue'
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      // msg: 'Welcome to Your Vue.js App'
+      login: true
+    }
+  },
+  components: {
+    Login,
+    Signup
+  },
+  computed: {
+    binding () {
+      const binding = {}
+      binding.column = true
+      if (this.$vuetify.breakpoint.mdAndUp) binding.column = false
+      return binding
     }
   }
 }
@@ -19,15 +46,11 @@ export default {
 <style scoped>
 h1, h2 {
   font-weight: normal;
+  text-align: center;
+  cursor: pointer;
+  margin: 5vh;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
 a {
   color: #42b983;
 }
---
+++ client/src/components/subcomponents/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <v-card class="elevation-12">
+      <v-toolbar dark color="teal darken-2">
+        <v-toolbar-title>Login</v-toolbar-title>
+        <v-spacer></v-spacer>
+      </v-toolbar>
+      <v-card-text>
+        <v-form>
+          <v-text-field prepend-icon="person" name="login" label="Email" type="email"></v-text-field>
+          <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="teal darken-2" class="white--text">Login</v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      drawer: null
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
